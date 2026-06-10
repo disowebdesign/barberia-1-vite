6b51ff717c625c261eef5522ad08fe3bfbe91294
disowebdesign <disowebdesign@gmail.com>
2026-06-09 22:45:36 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/barberia-1-vite/', // ← nombre exacto de tu repo en GitHub
+  base: '/barberia-1-vite/',
+  build: {
+    rollupOptions: {
+      input: {
+        main:      resolve(__dirname, 'index.html'),
+        trabajo:   resolve(__dirname, 'src/trabajo.html'),
+        productos: resolve(__dirname, 'src/productos.html'),
+        precios:   resolve(__dirname, 'src/precios.html'),
+        nosotros:  resolve(__dirname, 'src/nosotros.html'),
+      }
+    }
+  }
 })
